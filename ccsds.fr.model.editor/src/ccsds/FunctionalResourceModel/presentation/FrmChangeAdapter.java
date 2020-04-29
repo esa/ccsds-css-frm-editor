81fa07cd961c3f13c30b2841373c3eb7d50d77ad
Holger Dreihahn <holger.dreihahn@esa.int>
2020-04-29 10:32:48 +0200

    Resolve type references to local ASN.1 module for dragged (added) FRs.

--- ccsds.fr.model.editor/src/ccsds/FunctionalResourceModel/presentation/FrmChangeAdapter.java
+++ ccsds.fr.model.editor/src/ccsds/FunctionalResourceModel/presentation/FrmChangeAdapter.java
@@ -1,12 +1,20 @@
 package ccsds.FunctionalResourceModel.presentation;
 
 import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.common.util.TreeIterator;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.util.EContentAdapter;
 
+import ccsds.FunctionalResourceModel.FunctionalResource;
+import ccsds.FunctionalResourceModel.FunctionalResourceModel;
 import ccsds.FunctionalResourceModel.TypedElement;
+import ccsds.fr.type.model.frtypes.Element;
 import ccsds.fr.type.model.frtypes.Enumerated;
+import ccsds.fr.type.model.frtypes.Module;
 import ccsds.fr.type.model.frtypes.NamedValue;
 import ccsds.fr.type.model.frtypes.TypeDefinition;
+import ccsds.fr.type.model.frtypes.TypeReferenceLocal;
+import ccsds.fr.type.model.frtypes.util.FrTypesUtil;
 
 /**
  * Change adapter for an FRM model. Hooks up and initialises
@@ -27,9 +35,95 @@ public class FrmChangeAdapter extends EContentAdapter {
 			handleNewTypeDefinition((TypedElement)notification.getNotifier(), (TypeDefinition)notification.getNewValue());
 		}
 		
+		if(notification.getNewValue() instanceof FunctionalResource) {
+			FunctionalResource fr = (FunctionalResource)notification.getNewValue();				
+			adjustLocalTypeReferences(fr);
+		}
+		
 		super.notifyChanged(notification);
 	}
 
+	/**
+	 * Adjust the local type reference for this object and all children
+	 * @param obj
+	 */
+	private void adjustLocalTypeReferences(EObject obj) {
+		if(obj instanceof TypeReferenceLocal) {
+			TypeReferenceLocal lr = (TypeReferenceLocal)obj;
+			replaceTypeRefLocal(lr);
+		}
+		
+		TreeIterator<EObject> iter = obj.eAllContents();
+		while(iter.hasNext()) {
+			EObject child = iter.next();
+			adjustLocalTypeReferences(child);
+		}
+	}
+	
+	/**
+	 * Replace the type reference of the given local type reference if 
+	 * in the local ASN.1 type module has a type definition of the same name 
+	 * @param localReference	The local reference, which is adjusted to 
+	 * 							reference only type definitions from the 
+	 * 							local FRM ASN.1 module
+	 */
+	private void replaceTypeRefLocal(TypeReferenceLocal localReference) {
+		EObject parentContainer = localReference.eContainer();
+		Module localAsn1Module = null;
+		
+		// find the FRM and the ASN.1 module below
+		while(parentContainer.eContainer() != null) {
+			parentContainer = parentContainer.eContainer();
+			if(parentContainer != null && parentContainer instanceof FunctionalResourceModel) {
+				FunctionalResourceModel frm = (FunctionalResourceModel)parentContainer;
+				localAsn1Module = frm.getAsnTypeModule();
+				break;
+			}
+		}
+		
+		if(localAsn1Module != null) {
+			int typeIndex = 0;
+			boolean foundLocalTypeDef = false;
+			for(TypeDefinition localTypeDef : localAsn1Module.getTypeDefinition()) {
+				
+				// same type name, but objects are not the same, so it is from another FRM file (or there are more with same name) 
+				if(localTypeDef.getName() != null &&
+						localTypeDef.getName().equals(localReference.getTypeDefinition().getName()) && 
+								localTypeDef != localReference.getTypeDefinition()) {
+					String parent = localReference.eContainer().eClass().getName();
+					foundLocalTypeDef = true;
+					
+					if(localReference.eContainer() instanceof TypeDefinition) {
+						parent = ((TypeDefinition)localReference.eContainer()).getName();
+					}
+					
+					if(localReference.eContainer() instanceof Element) {
+						parent = ((Element)localReference.eContainer()).getName();
+					}
+					
+					FrTypesUtil.log("Replace type reference to other FRM file: " + localTypeDef.getName() + " at index " + typeIndex + " parent: " + parent);
+					
+					localReference.setTypeDefinition(localTypeDef);
+				} else if(localTypeDef.getName() != null &&
+						localTypeDef.getName().equals(localReference.getTypeDefinition().getName()) && 
+						localTypeDef == localReference.getTypeDefinition()) {
+					foundLocalTypeDef = true;
+				}
+				typeIndex++;
+			} // end for
+			
+			if(foundLocalTypeDef == false) {
+				try {
+					FrTypesUtil.warn("Could not replace type reference to other FRM file: " + localReference.getTypeDefinition().getName() +
+							" parent: " + localReference.eContainer());
+				} catch(Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+	}
+	
 	/**
 	 * Handles new NaemdValue children for enums and initialises with the next numeric value
 	 * @param notifier
